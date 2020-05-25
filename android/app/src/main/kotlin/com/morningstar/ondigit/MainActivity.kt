73da7f3ba80da5d6b3e6e9cb067ba3331f4f6863
FranckKouamelan <lekouamelan@gmail.com>
2020-05-25 14:45:25 +0000

    begin

---
+++ android/app/src/main/kotlin/com/morningstar/ondigit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.morningstar.ondigit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ondigit.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ondigit/screens/login.dart';
+
+ThemeData appTheme = ThemeData(
+    fontFamily: 'Oxygen'
+);
+
+class OnDigit extends StatefulWidget {
+
+  @override
+  _OnDigitState createState() => _OnDigitState();
+}
+
+class _OnDigitState extends State<OnDigit> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return MaterialApp(
+      title: 'On Digit',
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: new LoginSreen(),
+    );
+  }
+}
